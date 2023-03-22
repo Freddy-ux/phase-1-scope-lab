1d8aa132b04600bf256658e85d188e893f2c300c
Freddy-ux <alfredmuleri569@gmail.com>
2023-03-23 00:45:17 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// declaring a variable
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+   // modifying bob 
+    return customerName = customerName.toUpperCase();
+}
+
+// declaring a variable
+const leastFavoriteCustomer = 'Keith'
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Manuel'
+}
